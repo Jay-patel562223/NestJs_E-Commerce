092ca41d93daf9bcb06f087fcb560b7d12d07a63
JayPatel54 <jaypatel.qs@gmail.com>
2023-09-25 10:40:20 +0530

    First Commit

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AllExceptionFilter } from './httpExceptionFilter';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: 'APP_FILTER',
+      useClass: AllExceptionFilter,
+    },
+  ],
+})
+export class AppModule {}
